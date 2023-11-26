0bbc641e1ad028ce8bbeb90a0c455cc0ca16495a
AbdullahEsha <shahariaresha@gmail.com>
2023-11-26 12:04:41 +0600

    update

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import { store } from "@/redux/store";
 import { Provider } from "react-redux";
 
 export default function App({ Component, pageProps }) {
+  // Wraps the page in a Provider component to make the Redux store available to any nested components that need to access the Redux store.
   return (
     <Provider store={store}>
       <Component {...pageProps} />
