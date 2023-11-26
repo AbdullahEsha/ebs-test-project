9dae12073a07c723f415a39b436de5085ed3cea0
AbdullahEsha <shahariaresha@gmail.com>
2023-11-26 11:58:05 +0600

    token added

--- pages/users/index.js
+++ pages/users/index.js
@@ -3,13 +3,16 @@ import { useSelector, useDispatch } from "react-redux";
 const Users = () => {
   // get the user from the store
   const user = useSelector((state) => state.auth.user);
+  const token = useSelector((state) => state.auth.token);
 
   console.log("user", user);
+  console.log("token", token);
 
   return (
     <div className="p-32">
       <h1>Redux Toolkit Test</h1>
       <p>Hi {user.email} </p>
+      <p>token {token}</p>
     </div>
   );
 };
